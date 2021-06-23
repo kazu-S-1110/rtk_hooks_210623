bfebfb7a3140e8a60e71bf8621500a0e12c182a4
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-06-23 12:54:17 +0900

    storeへの追記。

--- src/features/task/taskSlice.js
+++ src/features/task/taskSlice.js
@@ -41,11 +41,8 @@ export const taskSlice = createSlice({
   extraReducers: (builder) => {},
 });
 
-export const { increment, decrement, incrementByAmount } = taskSlice.actions;
+export const { newTask, completeTask, deleteTask } = taskSlice.actions;
 
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectTask = (state) => state.counter.value;
+export const selectTasks = (state) => state.task.tasks;
 
 export default taskSlice.reducer;
