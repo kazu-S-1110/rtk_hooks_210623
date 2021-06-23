e095923e7fdc6ef18c33ba5a50a83558f86a43fd
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-06-23 12:39:55 +0900

    不要なコードを削除。reducerを作成。

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -5,7 +5,6 @@ import {
   increment,
   incrementByAmount,
   incrementAsync,
-  incrementIfOdd,
   selectCount,
 } from './counterSlice';
 import styles from './Counter.module.css';
@@ -54,12 +53,6 @@ export function Counter() {
           onClick={() => dispatch(incrementAsync(incrementValue))}
         >
           Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
         </button>
       </div>
     </div>
