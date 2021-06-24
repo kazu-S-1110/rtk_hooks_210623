14020968f40dc922ed35395b636ae47a15240c96
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-06-24 12:37:50 +0900

    extraReducerを定義。devtoolsで確認。photoだと失敗、、、？

--- src/features/fetch/fetchSlice.js
+++ src/features/fetch/fetchSlice.js
@@ -1,21 +1,31 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const apiUrl = 'https://jsonplaceholder.typicode.com/photos';
+const apiUrl = 'https://jsonplaceholder.typicode.com/users';
 
-const initialState = {
-  photos: [],
-};
-
-const fetchSlice = createSlice({
-  name: 'fetch',
-  initialState,
-  reducers: {},
-});
+// const initialState = {
+//   users: [],
+// };
 
 export const fetchAsyncGet = createAsyncThunk('fetch/get', async () => {
   const res = await axios.get(apiUrl);
   return res.data;
 });
+
+const fetchSlice = createSlice({
+  name: 'fetch',
+  initialState: { users: [] },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchAsyncGet.fulfilled, (state, action) => {
+      return {
+        ...state,
+        users: action.payload,
+      };
+    });
+  },
+});
+
+export const selectUser = (state) => state.fetch.users;
 
 export default fetchSlice.reducer;
